d552300cd446fa272dc330e96275f900720db53e
IT21232954 <it21232954@my.sliit.lk>
2023-05-06 16:29:12 +0530

    merged places UIs
    merged places UIs

---
+++ app/src/main/java/com/example/hotelcrud/NuwaraEliya.kt
@@ -0,0 +1,11 @@
+package com.example.hotelcrud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NuwaraEliya : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nuwara_eliya)
+    }
+}
---
+++ app/src/main/java/com/example/hotelcrud/edit_hotel.kt
@@ -0,0 +1,11 @@
+package com.example.hotelcrud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class edit_hotel : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_hotel)
+    }
+}
