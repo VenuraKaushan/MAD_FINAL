25f6c82214439d10b5e20043520f133a278124fe
Venura Kaushan <86141130+VenuraKaushan@users.noreply.github.com>
2023-05-13 23:47:21 +0530

    merge venura,beesara developed function

--- app/src/main/java/com/example/hotelcrud/homeui.kt
+++ app/src/main/java/com/example/hotelcrud/homeui.kt
@@ -1,28 +1,38 @@
 package com.example.hotelcrud
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class homeui : AppCompatActivity() {
 
     private lateinit var signup: Button
     private lateinit var signin: Button
+    private lateinit var adminLogin : FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homeui)
 
         signup = findViewById(R.id.signup)
         signin = findViewById(R.id.signin)
+        adminLogin = findViewById(R.id.adminLoginBtn)
 
         signup.setOnClickListener{
-           // val intent = Intent(this, registration::class.java)
+            val intent = Intent(this, registration::class.java)
             startActivity(intent)
         }
 
         signin.setOnClickListener{
             val intent = Intent( this,userlogin::class.java )
+            startActivity(intent)
+        }
+
+        adminLogin.setOnClickListener {
+            val intent = Intent( this,AdminLogin::class.java )
             startActivity(intent)
         }
     }
